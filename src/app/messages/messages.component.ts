8cfc0e01b1b3caa71c43d8b0644ba7bfd5a41fa7
nhuong <www.nhuong.zzz@gmail.com>
2022-06-01 08:55:20 +0700

    private service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,25 +7,20 @@ import { MessageService } from '../message.service';
   styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
-  constructor(private messageService: MessageService) {}
-
-  public messages: string[] = [];
-
-  public get Messages() {
-    return this.messageService.messages;
+  public messages: string[];
+  
+  
+  constructor(private messageService: MessageService) {
+    this.messages = messageService.messages;
+  
   }
-  public getMess(): void {
-    this.messages = this.Messages;
-  }
+  
+  
 
-  public get clearMethod() {
-    return this.messageService.clear;
-  }
-  public clear(): void {
-    this.clearMethod();
+  public clear() {
+    this.messageService.clear();
+    this.messages = this.messageService.messages;
   }
 
-  ngOnInit(): void {
-    this.getMess();
-  }
+  ngOnInit(): void {}
 }
